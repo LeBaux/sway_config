eee506a89c36ad4f6edfcc3ce8e260195e5463da
bryan <snakedye@protonmail.com>
2020-07-31 08:24:19 -0400

    arc-redesign

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 # Copy config files to .config folder
 
-cp -r ./sway ./termite/
+cp -r ./wallpapers ~/
+
+cp -r ./termite ~/.config/
+
+cp -r ./sway ~/.config/
 
 cp -r ./waybar ~/.config/
-
-
-cp -r ./wallpaper ~/
