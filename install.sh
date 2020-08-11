de2e6a1c81f7916686118839341b335cb6c696bb
bryan <snakedye@protonmail.com>
2020-08-11 07:55:17 -0400

    better_script

--- install.sh
+++ install.sh
@@ -1,13 +1,38 @@
+#!/bin/bash
+
+CONFIG=~/.config/
+
 # Copy config files to .config folder
+
+echo "The following directories are about to be move in ~/.config"
+echo "termite"
+echo "sway"
+echo "waybar"
+echo "wofi"
+echo -e "Do you want to continue? (y/n)"
+read ans
+
+unit [$ans = "yes]
+  do
+  if [$ans="yes" || $ans ="y"] 
+    then
+      echo "Moving directories"
+      break
+  fi
+  if [$ans="no" || $ans="n"]
+    then
+      exit
+    fi
+  done
 
 cp -r ./wallpapers ~/
 
-cp -r ./termite ~/.config/
+cp -r ./termite CONFIG
 
-cp -r ./sway ~/.config/
+cp -r ./sway CONFIG
 
-cp -r ./waybar ~/.config/
+cp -r ./waybar CONFIG
 
-cp -r ./wofi ~/.config/
+cp -r ./wofi CONFIG
 
 echo "Done!"
